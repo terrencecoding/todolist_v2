1817f6c6d9ebd2aaa6773c8719e8314d36909768
unknown <terrencetran2014@gmail.com>
2023-07-04 11:51:42 -0700

    Modify the the app.listen

--- app.js
+++ app.js
@@ -194,6 +194,6 @@ app.get("/about", function (req, res) {
 //   port = 3000;
 // }
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(3000, function () {
   console.log("Server has started successfully.");
 });
