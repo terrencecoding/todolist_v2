cbc09f53993bf6831333c939e8d99158119fd2a3
unknown <terrencetran2014@gmail.com>
2023-07-04 12:25:44 -0700

    installed dotenv and require in app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
+require("dotenv").config();
 
 const app = express();
 
